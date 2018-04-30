05a0d0703312673da490d9bdd21287e59d3d2049
emilo113 <emilo113@github.com>
2018-05-01 00:43:55 +0200

    Delivery info modal

---
+++ src/app/modals/delivery-info-modal/delivery-info-modal.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { AbstractModal } from '../abstract-modal';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { OrdersService } from '../../services/orders.service';
+import { AlertService } from '../../services/alert.service';
+
+@Component({
+    selector: 'app-delivery-info-modal',
+    templateUrl: './delivery-info-modal.component.html',
+    styleUrls: ['./delivery-info-modal.component.scss']
+})
+export class DeliveryInfoModalComponent extends AbstractModal implements OnInit {
+
+    @Input() order: any;
+
+    public deliveryDetails: any;
+
+    constructor(
+        public activeModal: NgbActiveModal,
+        private ordersService: OrdersService,
+        private alert: AlertService
+    ) {
+        super(activeModal);
+    }
+
+    ngOnInit() {
+        this.showLoader();
+        this.fetchInfo();
+    }
+
+    private fetchInfo(): void {
+        this.ordersService.fetchDeliveryDetails(this.order)
+            .subscribe(data => {
+                if (data instanceof Object) {
+                    console.log(data);
+                    this.deliveryDetails = data;
+                    this.hideLoader();
+                } else {
+                    this.alert.error('Something went wrong...');
+                    this.activeModal.close();
+                }
+            });
+    }
+
+}
